47c7079f37761f9959cb638dab3bccb6be8dfcd6
jasmine0714 <woooooooow22@gmail.com>
2024-02-19 17:58:14 +0900

    🔧 외부 이미지 로드시 images: remotePatters config 지정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,43 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/products/deleted_permanently",
+        destination: "/products",
+        permanent: true,
+      },
+      {
+        source: "/products/deleted_temporarily",
+        destination: "/products",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/products/jeans",
+        destination: "/products/1234",
+      },
+      {
+        source: "/products/shirts",
+        destination: "/products/1235",
+      },
+      {
+        source: "/products/shoes",
+        destination: "/products/1236",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
