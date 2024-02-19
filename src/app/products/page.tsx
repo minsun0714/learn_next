734729f28622e46947f2487730be256af6313e8f
jasmine0714 <woooooooow22@gmail.com>
2024-02-19 18:57:14 +0900

    🥅 빌드 에러 해결을 위해 products list를 파일에서 읽어오는 로직으로 변경

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,29 +1,30 @@
 import React from "react";
 import Link from "next/link";
-import { ProductType } from "../service/products";
+import { ProductType, getProducts } from "../service/products";
 import Image from "next/image";
 import clothesImage from "../../../public/images/clothes.jpg";
 
-const getProducts = async () => {
-  const response = await fetch("http://localhost:3000/api/products");
-  return await response.json();
-};
-
-// const getCatFacts = async () => {
-//   const response = await fetch("https://meowfacts.herokuapp.com/", {
-//     // 0으로 설정 시 SSG가 아니라 SSR처럼 동작
-//     next: { revalidate: 10 },
-//     // 또는 cache: "no-store"를 설정하여 캐시를 사용하지 않도록 설정하여도 SSR처럼 동작
-//   });
+// const getProducts = async () => {
+//   const response = await fetch("http://localhost:3000/api/products");
 //   return await response.json();
 // };
+
+const getCatFacts = async () => {
+  const response = await fetch("https://meowfacts.herokuapp.com/", {
+    // 0으로 설정 시 SSG가 아니라 SSR처럼 동작
+    next: { revalidate: 10 },
+    // 또는 cache: "no-store"를 설정하여 캐시를 사용하지 않도록 설정하여도 SSR처럼 동작
+  });
+  return await response.json();
+};
 const ProductsPage = async () => {
+  const products = await getProducts();
   const [
-    { res: products },
-    // , { data: catFacts }
+    // { res: products },
+    { data: catFacts },
   ] = await Promise.all([
-    getProducts(),
-    // getCatFacts(),
+    // getProducts(),
+    getCatFacts(),
   ]);
 
   return (
@@ -37,7 +38,7 @@ const ProductsPage = async () => {
           </li>
         ))}
       </ul>
-      {/* <p>{catFacts[0]}</p> */}
+      <p>{catFacts[0]}</p>
     </>
   );
 };
